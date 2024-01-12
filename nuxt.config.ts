308dc13f0c6c847749bbe45464a9b4f6e8c3e78a
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2024-01-12 22:09:10 +0100

    md & ios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   modules: ['@nuxtjs/ionic'],
+  ssr: false,
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   ionic: {
